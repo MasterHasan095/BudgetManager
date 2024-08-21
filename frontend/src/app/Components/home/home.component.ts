4f8ad2adff7a0eeb9285e96a8f900cb0d60bea57
DreamCoder <kau15124@sheridancollege.ca>
2024-08-21 14:24:08 -0400

    issue with url's

--- frontend/src/app/Components/home/home.component.ts
+++ frontend/src/app/Components/home/home.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { AuthService } from '../../Services/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  constructor(private router: Router) {}
 
+  navigateToLogin() {
+    this.router.navigate(['login']);
+  }
+
+  navigateToRegister() {
+    this.router.navigate(['register']);
+  }
 }
