d3e4dc49acc60e636e2a1462cf8f547348bc9dc6
masterha <masterha@sheridancollege.ca>
2024-08-21 12:35:47 -0400

    shifted routes to userRoutes

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,7 @@
 import express from "express";
 import { config } from "dotenv";
 import mongoose from "mongoose";
-import UserForBudget from "./models/userModelForBudget";
-import bcrypt from 'bcrypt';
-import jwt from "jsonwebtoken";
+import userRoutes from "./routes/userRoutes.js"
 config();
 const app = express();
 app.use(express.json());
@@ -27,61 +25,6 @@ app.get("/", (req, res) => {
   return res.sendStatus(234).send("Initial link");
 });
 
-app.get("/protected",authenticateToken, (rew, res) => {
-    return res.sendStatus(234).send("Protected");
-})
 
-app.post("/register", async (req, res) => {
-    const { username, password } = req.body;
-  
-    try {
-      const existingUser = await UserForBudget.findOne({ username });
-  
-      if (existingUser) {
-        return res.status(409).send("Username already taken");
-      }
-  
-      const hashedPassword = await bcrypt.hash(password, 10);
-      console.log(`Hashed : ${hashedPassword}`)
-      const newUser = new UserForBudget({ username, password: hashedPassword });
-      await newUser.save();
-      res.status(201).send("User registered");
-    } catch (error) {
-      res.status(500).send("Error registering user");
-    }
-  });
 
-app.post("/login", async (req, res) => {
-  const { username, password } = req.body;
-
-  try {
-    const user = await UserForBudget.findOne({ username });
-    if (!user) return res.status(404).send("User not found");
-
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid) return res.status(401).send("Invalid credentials");
-
-    console.log("ispasswordvalid")
-    const token = jwt.sign(
-      { username: user.username },
-      process.env.JWT_SECRET,
-      { expiresIn: "1h" }
-    );
-    console.log("tokencreated")
-    res.json({ token });
-  } catch (error) {
-    res.status(500).send("Error during login");
-  }
-});
-
-function authenticateToken(req, res, next) {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-  if (!token) return res.sendStatus(401);
-
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
-    req.user = user;
-    next();
-  });
-}
+app.use("/api/users", userRoutes);
---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,67 @@
+import UserForBudget from "../models/userModelForBudget";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import express from "express";
+
+
+const router = express.Router();
+
+
+router.post("/register", async (req, res) => {
+    const { username, password } = req.body;
+  
+    try {
+      const existingUser = await UserForBudget.findOne({ username });
+  
+      if (existingUser) {
+        return res.status(409).send("Username already taken");
+      }
+  
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const newUser = new UserForBudget({ username, password: hashedPassword });
+      await newUser.save();
+      res.status(201).send("User registered");
+    } catch (error) {
+      res.status(500).send("Error registering user");
+    }
+  });
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await UserForBudget.findOne({ username });
+    if (!user) return res.status(404).send("User not found");
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) return res.status(401).send("Invalid credentials");
+
+    const token = jwt.sign(
+      { username: user.username },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+    res.json({ token });
+  } catch (error) {
+    res.status(500).send("Error during login");
+  }
+});
+router.get("/authenticationTest",authenticateToken, (rew, res) => {
+    return res.sendStatus(234).send("Protected");
+})
+
+
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (!token) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
+
+export default router;
